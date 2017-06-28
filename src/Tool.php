4c23b8b979566f2617ea8bc3de99c25475781466
qiqisan <qiqisan1@126.com>
2017-06-28 17:59:39 +0800

    add setCtx

--- src/Tool.php
+++ src/Tool.php
@@ -1,8 +1,6 @@
 <?php
 namespace FSth\Co;
 
-use FSth\Co\Call;
-
 class Tool
 {
     public static function asyncSleep($ms)
@@ -11,6 +9,25 @@ class Tool
             swoole_timer_after($ms, function () use ($k) {
                 $k(null);
             });
+        });
+    }
+
+    public static function setCtx($key, $val)
+    {
+        return new SysCall(function (AsyncTask $task) use ($key, $val) {
+            while ($task->parent && $task = $task->parent) ;
+            $task->gen->generator->$key = $val;
+        });
+    }
+
+    public static function getCtx($key, $default = null)
+    {
+        return new SysCall(function (AsyncTask $task) use ($key, $default) {
+            while ($task->parent && $task = $task->parent) ;
+            if (isset($task->gen->generator->$key)) {
+                return $task->gen->generator->$key;
+            }
+            return $default;
         });
     }
 }
